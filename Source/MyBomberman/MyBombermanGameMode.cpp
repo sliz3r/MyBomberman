30af1547389c410a597524829007a1b759fe5424
David Cervera <dmarticervera@gmail.com>
2020-07-28 17:20:18 +0200

    Revert project clean-up

--- Source/MyBomberman/MyBombermanGameMode.cpp
+++ Source/MyBomberman/MyBombermanGameMode.cpp
@@ -7,7 +7,7 @@
 AMyBombermanGameMode::AMyBombermanGameMode()
 {
     // set default pawn class to our Blueprinted character
-    static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+    static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/MainPlayer"));
     if (PlayerPawnBPClass.Class != NULL)
     {
         DefaultPawnClass = PlayerPawnBPClass.Class;
