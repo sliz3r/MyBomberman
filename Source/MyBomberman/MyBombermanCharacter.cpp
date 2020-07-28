9f5cec71b2a7d281de349deedec5b31e650794d2
David Cervera <dmarticervera@gmail.com>
2020-07-28 08:09:58 +0200

    MyCharacterBomberman clean-up code and refactorized
    - Deleted unneeded code
    - Prepared code to instanciate bombs
    - Exposed timer between bombs

--- Source/MyBomberman/MyBombermanCharacter.cpp
+++ Source/MyBomberman/MyBombermanCharacter.cpp
@@ -14,110 +14,41 @@
 
 AMyBombermanCharacter::AMyBombermanCharacter()
 {
-    // Set size for collision capsule
     GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
 
-    // set our turn rates for input
-    BaseTurnRate = 45.f;
-    BaseLookUpRate = 45.f;
-
-    // Don't rotate when the controller rotates. Let that just affect the camera.
-    bUseControllerRotationPitch = false;
-    bUseControllerRotationYaw = false;
-    bUseControllerRotationRoll = false;
-
-    // Configure character movement
-    GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
-    GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f); // ...at this rotation rate
-    GetCharacterMovement()->JumpZVelocity = 600.f;
-    GetCharacterMovement()->AirControl = 0.2f;
-
-    // Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
-    // are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
+    GetCharacterMovement()->bOrientRotationToMovement = true;
+    GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f);
 }
 
-//////////////////////////////////////////////////////////////////////////
-// Input
-
-void AMyBombermanCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+void AMyBombermanCharacter::SetupPlayerInputComponent(class UInputComponent* playerInputComponent)
 {
-    // Set up gameplay key bindings
-    check(PlayerInputComponent);
-    PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
-    PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+    check(playerInputComponent);
+    playerInputComponent->BindAction("PlaceBomb", IE_Pressed, this, &AMyBombermanCharacter::PlaceBomb);
 
-    PlayerInputComponent->BindAxis("MoveForward", this, &AMyBombermanCharacter::MoveForward);
-    PlayerInputComponent->BindAxis("MoveRight", this, &AMyBombermanCharacter::MoveRight);
-
-    // We have 2 versions of the rotation bindings to handle different kinds of devices differently
-    // "turn" handles devices that provide an absolute delta, such as a mouse.
-    // "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
-    PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
-    PlayerInputComponent->BindAxis("TurnRate", this, &AMyBombermanCharacter::TurnAtRate);
-    PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
-    PlayerInputComponent->BindAxis("LookUpRate", this, &AMyBombermanCharacter::LookUpAtRate);
-
-    // handle touch devices
-    PlayerInputComponent->BindTouch(IE_Pressed, this, &AMyBombermanCharacter::TouchStarted);
-    PlayerInputComponent->BindTouch(IE_Released, this, &AMyBombermanCharacter::TouchStopped);
-
-    // VR headset functionality
-    PlayerInputComponent->BindAction("ResetVR", IE_Pressed, this, &AMyBombermanCharacter::OnResetVR);
+    playerInputComponent->BindAxis("MoveForward", this, &AMyBombermanCharacter::MoveForward);
+    playerInputComponent->BindAxis("MoveRight", this, &AMyBombermanCharacter::MoveRight);
 }
 
-
-void AMyBombermanCharacter::OnResetVR()
+void AMyBombermanCharacter::MoveForward(float value)
 {
-    UHeadMountedDisplayFunctionLibrary::ResetOrientationAndPosition();
+    Move(value, EAxis::X);
 }
 
-void AMyBombermanCharacter::TouchStarted(ETouchIndex::Type FingerIndex, FVector Location)
+void AMyBombermanCharacter::MoveRight(float value)
 {
-    Jump();
+    Move(value, EAxis::Y);
 }
 
-void AMyBombermanCharacter::TouchStopped(ETouchIndex::Type FingerIndex, FVector Location)
+void AMyBombermanCharacter::Move(float value, EAxis::Type axis)
 {
-    StopJumping();
-}
-
-void AMyBombermanCharacter::TurnAtRate(float Rate)
-{
-    // calculate delta for this frame from the rate information
-    AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
-}
-
-void AMyBombermanCharacter::LookUpAtRate(float Rate)
-{
-    // calculate delta for this frame from the rate information
-    AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
-}
-
-void AMyBombermanCharacter::MoveForward(float Value)
-{
-    if ((Controller != NULL) && (Value != 0.0f))
+    if ((Controller != NULL) && (value != 0.0f))
     {
-        // find out which way is forward
         const FRotator Rotation = Controller->GetControlRotation();
         const FRotator YawRotation(0, Rotation.Yaw, 0);
-
-        // get forward vector
-        const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
-        AddMovementInput(Direction, Value);
+        const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(axis);
+        AddMovementInput(Direction, value);
     }
 }
 
-void AMyBombermanCharacter::MoveRight(float Value)
-{
-    if ((Controller != NULL) && (Value != 0.0f))
-    {
-        // find out which way is right
-        const FRotator Rotation = Controller->GetControlRotation();
-        const FRotator YawRotation(0, Rotation.Yaw, 0);
-
-        // get right vector 
-        const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
-        // add movement in that direction
-        AddMovementInput(Direction, Value);
-    }
-}
+void AMyBombermanCharacter::PlaceBomb()
+{}
--- Source/MyBomberman/MyBombermanCharacter.h
+++ Source/MyBomberman/MyBombermanCharacter.h
@@ -14,47 +14,17 @@ class AMyBombermanCharacter : public ACharacter
 public:
     AMyBombermanCharacter();
 
-    /** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
+    /** Time to place bombs in seconds. */
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
-        float BaseTurnRate;
-
-    /** Base look up/down rate, in deg/sec. Other scaling may affect final rate. */
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
-        float BaseLookUpRate;
+        float TimeBetweenBoomPlacement = 1.0f;;
 
 protected:
+    void MoveForward(float value);
+    void MoveRight(float value);
+    void PlaceBomb();
+    virtual void SetupPlayerInputComponent(class UInputComponent* playerInputComponent) override;
 
-    /** Resets HMD orientation in VR. */
-    void OnResetVR();
-
-    /** Called for forwards/backward input */
-    void MoveForward(float Value);
-
-    /** Called for side to side input */
-    void MoveRight(float Value);
-
-    /**
-     * Called via input to turn at a given rate.
-     * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
-     */
-    void TurnAtRate(float Rate);
-
-    /**
-     * Called via input to turn look up/down at a given rate.
-     * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
-     */
-    void LookUpAtRate(float Rate);
-
-    /** Handler for when a touch input begins. */
-    void TouchStarted(ETouchIndex::Type FingerIndex, FVector Location);
-
-    /** Handler for when a touch input stops. */
-    void TouchStopped(ETouchIndex::Type FingerIndex, FVector Location);
-
-protected:
-    // APawn interface
-    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-    // End of APawn interface
-
+private:
+    void Move(float value, EAxis::Type axis);
 };
 
